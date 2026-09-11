3ee26c0c311c601d178f9cc9a79b78320602454c
Misto123 <contact@rebelinternet.eu>
2026-09-11 13:44:44 +0200

    Fix BAS proxy - URL encode username with semicolon

--- runner/bas-proxies.cjs
+++ runner/bas-proxies.cjs
@@ -1,18 +1,22 @@
 /**
  * BAS Proxy Configuration
  * Two Rebel mobile proxies for load balancing
+ * 
+ * Note: Username contains semicolon (ottovisits;p=1) which must be URL-encoded
  */
 
 const BAS_PROXIES = [
   {
     id: 'rebel-mobile-11',
     name: 'Rebel mobile 6903:8058',
-    proxy: 'http://ottovisits;p=1:LfjlMN)S*Cy74*_r@proxy2.rebelinternet.eu:5001'
+    // Username: ottovisits;p=1 (encoded as %3B for semicolon, %3D for equals)
+    // Password: LfjlMN)S*Cy74*_r (special chars allowed in password)
+    proxy: 'http://ottovisits%3Bp%3D1:LfjlMN)S*Cy74*_r@proxy2.rebelinternet.eu:5001'
   },
   {
     id: 'rebel-mobile-23',
     name: 'Rebel mobile 6903:8072',
-    proxy: 'http://ottovisits;p=2:LfjlMN)S*Cy74*_r@proxy2.rebelinternet.eu:5001'
+    proxy: 'http://ottovisits%3Bp%3D2:LfjlMN)S*Cy74*_r@proxy2.rebelinternet.eu:5001'
   }
 ];
 
